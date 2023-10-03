b10db021e93f941cf631155a306bf7b1ecc32f54
Duncanwar <semunda9@gmail.com>
2023-10-03 15:53:32 +0200

    done

--- counter/script.js
+++ counter/script.js
@@ -1,1 +1,21 @@
-const
+const num = document.getElementById("num");
+
+const reset = document.getElementById("res");
+const decrease = document.getElementById("dec")
+const increase = document.getElementById("inc")
+
+let numVal = document.getElementById("num").textContent;
+
+decrease.addEventListener("click",()=>{
+    numVal = parseInt(numVal)-1;
+    num.textContent= numVal
+})
+
+increase.addEventListener("click",()=>{
+    numVal = parseInt(numVal)+1;
+    num.textContent= numVal
+})
+
+reset.addEventListener("click",()=>{
+    num.textContent= 0
+})
